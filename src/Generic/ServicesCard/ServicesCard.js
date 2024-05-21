ce68dfa0993cfcc1fb6ea89e2b8c3e3200db92e2
Sandeep Sharma <112006947+sandee2847@users.noreply.github.com>
2024-05-21 12:04:21 +0530

    updated:

---
+++ src/Generic/ServicesCard/ServicesCard.js
@@ -0,0 +1,37 @@
+import React, { useState } from "react";
+import "./ServicesCard.css";
+import Icon from "../../Assets/image 3icon.svg";
+import RightArrow from "../../Assets/Arrow Right.svg";
+
+import { Data } from "../../Constants/Data/Data";
+
+function ServicesCard({ data, hover = false }) {
+  const [onHover, setOnHover] = useState(false);
+  return (
+    <div
+      className="ServicescardContainer flex flex-column flex-center"
+      onMouseOver={() => setOnHover(true)}
+      onMouseOut={() => setOnHover(false)}
+    >
+      <div className="w">
+        {onHover ? (
+          <img src={data.hoverImage} className="service-icon" />
+        ) : (
+          <img src={data.image} className="service-icon" />
+        )}
+      </div>
+      <div className="text-center">
+        <p className="title-text">{data.title}</p>
+        <p className="cardText text text-left">{data.discription}</p>
+      </div>
+      <div className="button-text Learn-more flex flex-center gap2">
+        Learn more{" "}
+        <span>
+          <img className="arrow" src={RightArrow} alt=">" />
+        </span>
+      </div>
+    </div>
+  );
+}
+
+export default ServicesCard;
